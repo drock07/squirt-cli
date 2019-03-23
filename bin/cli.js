74e570ff956516579f918599c7d383a28d1e3cd7
David Rockwood <david.w.rockwood@gmail.com>
2019-03-22 19:00:11 -0600

    Implement working, though messy, cli

--- bin/cli.js
+++ bin/cli.js
@@ -7,6 +7,8 @@ const path = require('path')
 const fs = require('fs-extra')
 const findup = require('findup-sync')
 const async = require('async')
+const inquirer = require('inquirer')
+const tmpl = require('blueimp-tmpl')
 
 program
   .version('0.0.1', '-v, --version')
@@ -33,10 +35,10 @@ function validateTemplatesPath(configDir, config, cb) {
 
   if (!config.templatesPath) {
     pathsToCheck.push(path.resolve(configDir, 'squirtTemplates'))
-  } else if (Array.isArray(config.templatesPath)) {
-    config.templatesPath.forEach(templatePath => pathsToCheck.push(path.resolve(templatePath)))
+  // } else if (Array.isArray(config.templatesPath)) {
+  //   config.templatesPath.forEach(templatePath => pathsToCheck.push(path.resolve(templatePath)))
   } else {
-    pathsToCheck.push(path.resolve(config.templatesPath))
+    pathsToCheck.push(path.resolve(configDir, config.templatesPath))
   }
 
   async.each(pathsToCheck, (pathToCheck, callback) => {
@@ -63,25 +65,87 @@ function mergeConfig(config, cb) {
 
 // CLI
 // get template from arg OR launch template chooser prompt
+function getTemplate(config, cb) {
+  let template = config.templates[program.args[0]]
 
+  if (!template) {
+    inquirer.prompt([{
+      type: 'list',
+      name: 'templateName',
+      message: 'Which template would you like to squirt?',
+      choices: Object.keys(config.templates)
+    }]).then(answers => {
+      cb(null, config, config.templates[answers.templateName])
+    })
+  } else {
+    cb(null, config, template)
+  }
+
+}
 
 // CLI
 // prompt template values from user
+function getTemplateValues(config, template, cb) {
+  inquirer.prompt(template.script).then(answers => {
+    cb(null, config, template, answers)
+  }).catch(cb)
+}
 
 // LIB
 // generate files from templates
+function buildFilesToWrite(config, templateInfo, answers, cb) {
+  async.map(Object.keys(templateInfo.files), (templateFilePath, done) => {
+    async.waterfall([
+      function readFile(waterfallCallback) {
+        const fullTemplateFilePath = path.resolve(config.templatesPath[0], templateFilePath)
+        fs.readFile(fullTemplateFilePath, (err, data) => {
+          waterfallCallback(err, data.toString())
+        })
+      },
+      function buildTemplate(templateString, waterfallCallback) {
+        try {
+          const builtTemplate = tmpl(templateString, answers)
+          waterfallCallback(null, builtTemplate)
+        } catch (err) {
+          waterfallCallback(err)
+        }
+      }
+    ], (_, result) => {
+      const outputPath = templateInfo.files[templateFilePath]
+      try {
+        const builtOutputPath = tmpl(outputPath, answers)
+        const fullBuiltOutputPath = path.resolve(builtOutputPath)
 
-// CLI?
+        done(null, {
+          path: fullBuiltOutputPath,
+          contents: result
+        })
+      } catch (err) {
+        done(err)
+      }
+    })
+  }, (err, filesToWrite) => {
+    cb(err, config, filesToWrite)
+  })
+}
+
 // write files
+function writeFiles(config, filesToWrite, cb) {
+  async.each(filesToWrite, (fileInfo, eachCallback) => {
+    fs.outputFile(fileInfo.path, fileInfo.contents, eachCallback)
+  }, err => {
+    cb(err, config)
+  })
+}
 
 async.waterfall([
   loadConfig,
   validateTemplatesPath,
   mergeConfig,
-  (config, cb) => {
-    console.log(config)
-    cb()
-  }
+  getTemplate,
+  getTemplateValues,
+  buildFilesToWrite,
+  writeFiles
 ], function (err) {
   if (err) {
     console.log(err)
