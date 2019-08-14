b40e6d0c8af9d4b007502fa90692878740a71658
David Rockwood <david.w.rockwood@gmail.com>
2019-08-14 17:02:10 -0600

    Refactor and simplify cli, add init

--- bin/cli.js
+++ bin/cli.js
@@ -2,25 +2,4 @@
 
 'use strict';
 
-const program = require('commander')
-const async = require('async')
-
-program
-  .version('0.0.1', '-v, --version')
-  .arguments('[templateName] [templateArguments]')
-  .option('-c, --config <configPath>', 'Specify a config file')
-  // .option('-d, --default', 'Use default template values where available')
-  .option('-tp, --templatesPath', 'Set the path to the templates')
-  .parse(process.argv)
-
-const buildConfig = require('../lib/buildSquirtConfig')(program, process.cwd())
-const Squirt = require('../lib/Squirt')(program, process.cwd())
-
-async.waterfall([
-  buildConfig,
-  Squirt
-], function (err) {
-  if (err) {
-    console.log(err)
-  }
-})
+require('../lib/cli')(process.argv)
